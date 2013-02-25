132693c9eeeecc8537f1198fbf08fd832f0943bc
Michael Hoffer <info@michaelhoffer.de>
2013-02-25 22:09:56 +0100

    subproject added

---
+++ VRLInstrumentation/src/eu/mihosoft/vrl/instrumentation/VRLInstrumentationTransformation.java
@@ -0,0 +1,275 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eu.mihosoft.vrl.instrumentation;
+
+import org.codehaus.groovy.transform.ASTTransformation;
+import org.codehaus.groovy.transform.GroovyASTTransformation;
+import org.codehaus.groovy.control.SourceUnit;
+import org.codehaus.groovy.control.CompilePhase;
+import org.codehaus.groovy.ast.ASTNode;
+import org.codehaus.groovy.ast.ClassCodeExpressionTransformer;
+import org.codehaus.groovy.ast.ClassNode;
+import org.codehaus.groovy.ast.CodeVisitorSupport;
+import org.codehaus.groovy.ast.MethodNode;
+import org.codehaus.groovy.ast.expr.ArgumentListExpression;
+import org.codehaus.groovy.ast.expr.BinaryExpression;
+import org.codehaus.groovy.ast.expr.ClosureExpression;
+import org.codehaus.groovy.ast.expr.ConstantExpression;
+import org.codehaus.groovy.ast.expr.Expression;
+import org.codehaus.groovy.ast.expr.MethodCallExpression;
+import org.codehaus.groovy.ast.expr.StaticMethodCallExpression;
+import org.codehaus.groovy.ast.expr.VariableExpression;
+import org.codehaus.groovy.ast.stmt.BlockStatement;
+import org.codehaus.groovy.classgen.Verifier;
+
+/**
+ * Adds instrumentation to each method call. Use {@link VRLInstrumentation} to
+ * request this transformation.
+ *
+ * @author Michael Hoffer <info@michaelhoffer.de>
+ */
+@GroovyASTTransformation(phase = CompilePhase.SEMANTIC_ANALYSIS)
+public class VRLInstrumentationTransformation implements ASTTransformation {
+
+    @Override
+    public void visit(ASTNode[] astNodes, SourceUnit sourceUnit) {
+        
+        System.out.println("VISIT: ");
+
+        // create transformer instance
+        MethodCallExpressionTransformer transformer =
+                new MethodCallExpressionTransformer(sourceUnit);
+
+//        InstrumentVisitor visitor = new InstrumentVisitor(null);
+
+
+        // apply transformation for each class in the specified source unit
+        for (ClassNode clsNode : sourceUnit.getAST().getClasses()) {
+            transformer.visitClass(clsNode);
+
+
+//            for (MethodNode m : clsNode.getMethods()) {
+//                if (m.getCode() instanceof BlockStatement) {
+//                    visitor.visitBlockStatement((BlockStatement) m.getCode());
+//                }
+//            }
+
+        }
+    } 
+}
+
+//class InstrumentVisitor extends CodeVisitorSupport {
+//
+////    @Override
+////    public void visitMethodCallExpression(MethodCallExpression methodCall) {
+////        methodCall.
+////    }
+//    
+//    public Expression transform(Expression exp) {
+//        
+//        System.out.println(" --> transform: " + exp);
+//        
+//        // don't transform if expression is null
+//        if (exp == null) {
+//            return exp;
+//        }
+//
+//        // we ignore other expressions as we only want to transform/instrument
+//        // method calls
+//        if (!(exp instanceof MethodCallExpression)) {
+//            return exp;
+//        }
+//
+//        // we have a method call
+//        MethodCallExpression methodCall = (MethodCallExpression) exp;
+//
+//        // if the method is the special each(..) method which does not seem
+//        // to belong to a specific type, show warning and ignore it,
+//        // including it's closure
+//        if (methodCall.getMethod().getText().equals("each")) {
+//            System.out.println(">> WARNING: each() cannot be instrumented (unsupported):");
+//            System.out.println(" --> begin: line " + exp.getLineNumber() + ", col " + exp.getColumnNumber());
+//            System.out.println(" --> end:   line " + exp.getLastLineNumber() + ", col " + exp.getLastColumnNumber());
+//
+//            return exp;
+//        }
+//
+//        // name of the object
+//        String objName = methodCall.getObjectExpression().getText();
+//        // name of the type of the object
+//        String typeName = methodCall.getObjectExpression().getType().getName();
+//
+//        // if both are equal, static method call detected
+//        boolean staticCall = typeName.equals(objName);
+//
+//        // create a new argument list with object the method belongs to
+//        // as first parameter and the method name as second parameter
+//        ArgumentListExpression finalArgs = new ArgumentListExpression(
+//                new ConstantExpression(staticCall),
+//                methodCall.getObjectExpression(),
+//                new ConstantExpression(methodCall.getMethod().getText()));
+//
+//        // original method args
+//        ArgumentListExpression mArgs =
+//                (ArgumentListExpression) methodCall.getArguments();
+//
+//        // add original method args to argument list of the instrumentation
+//        // method
+//        for (Expression e : mArgs.getExpressions()) {
+//            finalArgs.addExpression(e);
+//        }
+//
+//        // create a static method call to the instrumentation method which
+//        // calls the original method via reflection
+//        return new StaticMethodCallExpression(
+//                new ClassNode(GroovyAST01.class),
+//                "__instrumentCode", finalArgs);
+//    }
+//}
+/**
+ * Concrete transformer class.
+ *
+ * @author Michael Hoffer <info@michaelhoffer.de>
+ */
+class MethodCallExpressionTransformer extends ClassCodeExpressionTransformer {
+
+    private final SourceUnit unit;
+
+    public MethodCallExpressionTransformer(final SourceUnit unit) {
+        this.unit = unit;
+    }
+
+    @Override
+    public Expression transform(Expression exp) {
+
+        System.out.println(" --> transform: ");// + exp);
+
+        // don't transform if expression is null
+        if (exp == null) {
+            System.out.println(" --> NULL");
+            return exp;
+        }
+
+
+        // we ignore other expressions as we only want to transform/instrument
+        // method calls
+        if (!(exp instanceof MethodCallExpression)) {
+            System.out.println(" --skipping expression: " + exp.getClass());
+
+            return exp.transformExpression(this);
+        }
+
+        // we have a method call
+        MethodCallExpression methodCall = (MethodCallExpression) exp;
+
+        // original method args
+        ArgumentListExpression mArgs =
+                (ArgumentListExpression) methodCall.getArguments();
+
+        // instrument method args (possible method calls inside)
+        ArgumentListExpression instrumentedMargs = new ArgumentListExpression();
+        for (Expression mArg : mArgs.getExpressions()) {
+            instrumentedMargs.addExpression(mArg.transformExpression(this));
+        }
+        mArgs = instrumentedMargs;
+
+
+        // if the method is the special each(..) method which does not seem
+        // to belong to a specific type, show warning and ignore it,
+        // including it's closure
+        if (methodCall.getMethod().getText().equals("each")) {
+            System.out.println(">> WARNING: each() cannot be instrumented (unsupported):");
+            System.out.println(" --> begin: line " + exp.getLineNumber() + ", col " + exp.getColumnNumber());
+            System.out.println(" --> end:   line " + exp.getLastLineNumber() + ", col " + exp.getLastColumnNumber());
+
+            return exp;
+        } else {
+            System.out.println(">> instrumenting method " + methodCall.getMethod().getText() + " : " + methodCall.getClass());
+        }
+
+
+        // name of the object
+        String objName = methodCall.getObjectExpression().getText();
+        // name of the type of the object
+        String typeName = methodCall.getObjectExpression().getType().getName();
+
+        // if both are equal, static method call detected
+        boolean staticCall = typeName.equals(objName);
+
+        // create a new argument list with object the method belongs to
+        // as first parameter and the method name as second parameter
+        ArgumentListExpression finalArgs = new ArgumentListExpression(
+                new ConstantExpression(staticCall),
+                methodCall.getObjectExpression(),
+                new ConstantExpression(methodCall.getMethod().getText()));
+
+
+        // add original method args to argument list of the instrumentation
+        // method
+        for (Expression e : mArgs.getExpressions()) {
+
+            finalArgs.addExpression(e);
+        }
+
+        // create a static method call to the instrumentation method which
+        // calls the original method via reflection
+        return new StaticMethodCallExpression(
+                new ClassNode(VRLInstrumentationUtil.class),
+                "__instrumentCode", finalArgs);
+    }
+
+    @Override
+    protected SourceUnit getSourceUnit() {
+        return unit;
+    }
+}
+
+class InstrumentVisitor extends CodeVisitorSupport {
+
+//        private Map<MethodNode, BlockStatement> methodBodies = new HashMap<>();
+//        private VariableScope parentScope;
+    private MethodNode methodNode;
+
+    public InstrumentVisitor(MethodNode methodNode) {
+        this.methodNode = methodNode;
+    }
+
+    @Override
+    public void visitBlockStatement(BlockStatement bs) {
+//            System.out.println("enter block: " + bs.getText());
+//            parentScope = bs.getVariableScope();
+//            getMethodBodies().put(methodNode, bs);
+        super.visitBlockStatement(bs);
+    }
+
+    @Override
+    public void visitMethodCallExpression(MethodCallExpression methodCall) {
+
+        methodCall.visit(new MethodCallExpressionTransformer(null));
+
+        System.out.println("enter method: " + methodCall.getText());
+
+        System.out.println(" --> method: " + methodCall.getMethod().getText());
+
+        System.out.println(" --> object: " + methodCall.getObjectExpression().getText());
+        System.out.println(" --> type: " + methodCall.getObjectExpression().getType().getName());
+
+        String objName = methodCall.getObjectExpression().getText();
+        String typeName = methodCall.getObjectExpression().getType().getName();
+
+
+//            boolean isClassObject = typeName.equals(objName);
+//
+//            if (isClassObject) {
+//                System.out.println(" --> static method");
+//            } else {
+//                System.out.println(" --> object method");
+//            }
+
+
+//            super.visitBlockStatement(createInstrumentedCall("start " + methodCall.getText(), "stop " + methodCall.getText(), methodCall, parentScope));
+//            super.visitMethodCallExpression(methodCall);
+    }
+}
---
+++ VRLInstrumentation/src/eu/mihosoft/vrl/instrumentation/VRLInstrumentationUtil.java
@@ -0,0 +1,141 @@
+
+package eu.mihosoft.vrl.instrumentation;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Michael Hoffer <info@michaelhoffer.de>
+ */
+public class VRLInstrumentationUtil {
+  // http://groovy.codehaus.org/Compile-time+Metaprogramming+-+AST+Transformations
+    // http://hamletdarcy.blogspot.de/2009/01/groovy-compile-time-meta-magic.html
+    // methodcall transformations:
+    // http://groovy.329449.n5.nabble.com/AST-Transforming-a-MethodCallExpression-td5711841.html
+    // type transformations:
+    // http://groovy.329449.n5.nabble.com/is-possible-an-AST-transformation-to-convert-all-BigDecimals-to-doubles-in-GroovyLab-td5711461.html
+
+
+    /**
+     * Do not call manually! This method will be used by AST transformations
+     * to instrument method calls.
+     * @param staticCall defines whether method call is a static method call
+     * @param o instance the method belongs to
+     * @param mName method name
+     * @param args method arguments
+     * @return return value of the method that shall be instrumented
+     * @throws Throwable 
+     */
+    public static Object __instrumentCode(boolean staticCall, Object o, String mName, Object[] args) throws Throwable {
+
+        ArrayList<Class<?>> paramTypes = new ArrayList<Class<?>>();
+
+        System.out.println("calling " + mName + "(...)");
+
+        System.out.println(" --> o: " + o + " : " + o.getClass());
+
+        for (Object p : args) {
+            System.out.println(" --> arg: " + p + " : " + p.getClass());
+            Class<?> c = p.getClass();
+            paramTypes.add(c);
+        }
+        
+        // updateVisualization(o, mName, args);
+
+        try {
+
+            Class<?> cls;
+
+            if (staticCall) {
+                cls = (Class<?>) o;
+//                System.out.println(" -> static: " + staticCall);
+            } else {
+                cls = o.getClass();
+//                System.out.println(" -> static: " + staticCall);
+            }
+
+            Method method = null;
+
+            try {
+                method = cls.getMethod(mName, paramTypes.toArray(new Class<?>[]{}));
+            } catch (NoSuchMethodException ex) {
+                //
+            }
+
+            if (method == null) {
+
+                for (Method m : cls.getMethods()) {
+
+                    if (!m.getName().equals(mName)) {
+                        continue;
+                    }
+
+                    Class<?>[] methodParams = m.getParameterTypes();
+
+                    if (paramTypes.size() != methodParams.length) {
+                        continue;
+                    }
+
+                    boolean compatibleParameterTypes = true;
+
+                    for (int i = 0; i < paramTypes.size(); i++) {
+                        if (!VClassLoaderUtil.convertPrimitiveToWrapper(paramTypes.get(i)).getName().
+                                equals(VClassLoaderUtil.convertPrimitiveToWrapper(methodParams[i]).getName())) {
+                            compatibleParameterTypes = false;
+                            break;
+                        }
+                    }
+
+                    if (compatibleParameterTypes) {
+                        method = m;
+                        break;
+                    }
+                }
+            }
+
+            // last attempt to fix the method, maybe println() without System.out
+            if (method == null) {
+                if (mName.equals("print") || mName.equals("println")) {
+                    try {
+                        method = System.out.getClass().getMethod(mName, String.class);
+                        o = System.out;
+                    } catch (NoSuchMethodException ex) {
+                        //
+                    }
+                }
+            }
+
+
+            // access this method even if language visibility forbids
+            // invocation of this method
+            method.setAccessible(true);
+
+            Object returnValue =  method.invoke(o, (Object[]) args);
+            
+            // updateVisualization(o, mName, returnValue);
+            
+            return returnValue;
+
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(VRLInstrumentationUtil.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalArgumentException ex) {
+            Logger.getLogger(VRLInstrumentationUtil.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InvocationTargetException ex) {
+            Logger.getLogger(VRLInstrumentationUtil.class.getName()).log(Level.SEVERE, null, ex);
+            throw ex.getCause();
+        } catch (SecurityException ex) {
+            Logger.getLogger(VRLInstrumentationUtil.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return null;
+    }
+}
