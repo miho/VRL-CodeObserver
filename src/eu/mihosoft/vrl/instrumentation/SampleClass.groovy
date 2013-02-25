214cb403cfed650e4156b48ce6316f333185c435
Michael Hoffer <info@michaelhoffer.de>
2013-02-25 22:10:26 +0100

    multiproject setup working

---
+++ src/eu/mihosoft/vrl/instrumentation/SampleClass.groovy
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eu.mihosoft.vrl.instrumentation;
+
+/**
+ *
+ * @author Michael Hoffer <info@michaelhoffer.de>
+ */
+@VRLInstrumentation 
+class SampleClass { 
+
+    def greet() {  
+        
+       System.out.println("GROOVY: " + b(3) + " : " + groovy.lang.GroovySystem.getVersion()); 
+        
+       System.out.println("Hello 1"); 
+         
+       System.out.println("Hello 2"); 
+        
+        int n = 3; 
+        
+        for(int i = 0; c(i,n); i++) { 
+             
+            int val = Math.abs(i-n*2);
+            
+            (1..10).each {
+                it->println("closure i: " + i)
+            }  
+             
+            
+            System.out.println(" --> i: " + i + " = " + b(val));
+//            
+            b(i);    
+        }                  
+    }    
+      
+    public int b(int i){println("i: " + i); return i*i;};  
+    
+    public boolean c(int i, int n) {
+            return i < n;
+    }
+}
+   
+
+   
