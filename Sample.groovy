ee2b406febe3225b13ac459742384494e33c4d03
Michael Hoffer <info@michaelhoffer.de>
2013-07-03 20:30:40 +0200

    sample changed (much simpler, trying to debug dataflow)

--- Sample.groovy
+++ Sample.groovy
@@ -18,39 +18,26 @@ class SampleClass {
     SampleClass() {
     }
 
-    def greet() {  
-        
-       System.out.println("GROOVY: " + b(b(b(3-8)+b(2))) + " : " + groovy.lang.GroovySystem.getVersion()); 
-        
-        int n = 3; 
-        
-		VSource.id(23)
-        for(int i = 0; c(i,10); i++) { 
-            
-            int val = Math.abs(i-n*2);
-            
-            (1..10).each {
-                it->println("closure i: " + b(i))
-            }  
-             
-            
-            System.out.println(" --> i: " + i + " = " + b(val));
-//            
-            b(i);    
-        }       
-        
-		VSource.id(123)
-        System.out.println("FINAL CALL");
-    }    
-      
-    public int b(int i){println("b: " + i); return i*i;};  
     
-    public boolean c(int i, int n) {
-
-            return i < n;
+    void playground() {
+      
+      int a = 1
+      int b = 2
+      
+      methodTwo(methodOne(a),methodOne(b))
+      
+    }
+    
+     int methodOne(int a) {  
+	return a;
+    }
+    
+    int methodTwo(int a, int b) {
+	return a+b;
     }
    
 }
+
    
 
    
